44537a3dd1d6106fd6d4f00b57cb14edf15adb1e
Antonio Ruiz <pepecode1@gmail.com>
2025-09-05 11:45:49 -0600

    Initial Commit

---
+++ swiftui-todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-todo
+//
+//  Created by Pepe Ruiz on 05/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-todo/swiftui_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_todoApp.swift
+//  swiftui-todo
+//
+//  Created by Pepe Ruiz on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
